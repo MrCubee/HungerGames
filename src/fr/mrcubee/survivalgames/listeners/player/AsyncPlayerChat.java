5ed0cdeebae7d568ce7b6c9ea349cdd4d8765512
MrCubee <contact@mrcubee.fr>
2021-10-27 19:55:09 +0200

    Add a prefix in the spectator chat.

--- src/fr/mrcubee/survivalgames/listeners/player/AsyncPlayerChat.java
+++ src/fr/mrcubee/survivalgames/listeners/player/AsyncPlayerChat.java
@@ -1,16 +1,12 @@
 package fr.mrcubee.survivalgames.listeners.player;
 
+import fr.mrcubee.langlib.Lang;
 import fr.mrcubee.survivalgames.Game;
-import fr.mrcubee.survivalgames.GameStats;
 import fr.mrcubee.survivalgames.SurvivalGames;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class AsyncPlayerChat implements Listener {
 
@@ -20,19 +16,15 @@ public class AsyncPlayerChat implements Listener {
 		this.survivalGames = survivalGames;
 	}
 	
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGH)
 	public void asyncPlayerChat(AsyncPlayerChatEvent event) {
 		Game game;
 
 		game = this.survivalGames.getGame();
 		if (!game.isSpectator(event.getPlayer()))
 			return;
-		event.setCancelled(true);
-		this.survivalGames.getServer().getOnlinePlayers().stream().filter(game::isSpectator).forEach(player -> {
-			String message = String.format(event.getFormat(), event.getPlayer().getName(), event.getMessage());
-
-			player.sendMessage(ChatColor.GRAY + message);
-		});
+		event.setFormat(Lang.getMessage(event.getPlayer(), "chat.spectator.prefix", "&7[Spectator]&r", true) + event.getFormat());
+		event.getRecipients().removeIf((player) -> !game.isSpectator(player));
 	}
 	
 }
